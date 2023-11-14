e7d4c48ae7eb030ffb83cc445f67c1641f83167f
Asma Siyabi <asma.alsiyabi2000@gmail.com>
2023-11-14 14:38:44 +0400

    new activity

---
+++ test.py.py
@@ -0,0 +1,17 @@
+#the wall length
+WellLeng= 100
+#the white and lack tile
+TileLeng=5
+tilsNo= WellLeng/TileLeng
+Black_Tils=tilsNo/2
+print ("No of black tils = " , Black_Tils)
+White_Tils= Black_Tils-1
+print ("No of white tils = ", White_Tils)
+total_No= Black_Tils+ White_Tils
+print ("Total Number of tils =", total_No)
+
+  
+#To count the gap, count the tile lingth/2
+#couting the gap
+gap= TileLeng/2
+print ("gap =", gap) 
