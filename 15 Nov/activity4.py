e04e119d435beb5a5ddec6291b58d812e3f7a4a8
Asma Siyabi <asma.alsiyabi2000@gmail.com>
2023-11-15 15:50:25 +0400

    time activity

--- 15 Nov/activity4.py
+++ 15 Nov/activity4.py
@@ -1,13 +1,14 @@
 time=float(input("Enter time : "))
+minutes= int(input("Enter minutes: "))
 partOfDay = input("Is it AM or PM?")
 if (partOfDay.lower() == "am"):
- if (time<8):
+ if time<8 or (time ==8 and minutes == 0):
   print("before 8:00 Am")
  else: 
     print("8:00 AM")
 elif (partOfDay.lower() == "pm"):
-     if (time<8):
-      print ("before 8:00 Am")
+     if time<8 or (time ==8 and minutes == 0):
+      print ("After 8:00 Am")
      else: 
          print("Invlid")
     
