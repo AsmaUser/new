612139fe5a981e16b828ffbe7d55963535312f40
Asma Siyabi <asma.alsiyabi2000@gmail.com>
2023-11-26 13:45:25 +0400

    Dictionery activity

---
+++ 26 Nov/create 2 tuples.py
@@ -0,0 +1,7 @@
+def func1(t):
+    for i in range (len(t)):
+        t = [t]*2
+t = (1,2,3)
+print(t)
+func1(t)
+print(t)
---
+++ 26 Nov/dictionaries (count age ).py
@@ -0,0 +1,7 @@
+dec = {1:{'name':'John', 'age':'27', 'sex':'Male'},
+       2:{'name':'Maria', 'age':'22', 'sex':'Female'},
+       3:{'name':'Sali', 'age':'23', 'sex':'Male'}}
+
+for key in dec:
+    if dec[key]['age']>'22': 
+        print (dec[key]["name"])
