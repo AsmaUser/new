3df7c6bd082f19a6362830d057ce1fb42114549b
Asma Siyabi <asma.alsiyabi2000@gmail.com>
2023-11-29 10:13:47 +0400

    new

--- 28 Nov/class activity5.py
+++ 28 Nov/class activity5.py
@@ -14,29 +14,32 @@ class Circal(Ship):
     def __init(self, name, color,r):
         super().__init__( name, color)
         self.r=r
-    def say_ship(self):
-        return("The ship are {} and color is {} ".format(self.name , self.color))
-        
+    
     def area_ship(self):
         area =(3.14*(self.r**2))
         return area 
 
+    def say_ship(self):
+        return("The ship are {} and color is {} ".format(self.name , self.color))
+        
+   
 class Squir(Ship):
     def __init(self, name, color,l):
         super().__init__(name, color)
         self.l=l
-    def say_ship(self):
-        print ("The ship are {} and color is {} ".format(self.name , self.color))
-    
     def area_ship2(self):
         area2 =self.l**2
-        return area2         
-                
+        return area2
+    
+    def say_ship(self):
+        return("The ship are {} and color is {} ".format(self.name,self.color))
+    
+                      
 p1= Ship("yelow", "sh2")
-p2= Squir("circal", "Pink")
-s1= Circal("squir", "Yellow")
+p2= Squir("squir", "Pink")
+s1= Circal("circl", "Yellow")
 print(p1)
 print(p2)
 print(s1)
-print ("Area= ", p1.area())
-print ("Area=  ", p2.area2())
+print ("Area= ", s1.area_ship())
+print ("Area=  ", p2.area_ship2())
